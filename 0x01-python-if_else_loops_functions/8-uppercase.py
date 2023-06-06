22f1ae74944a554d39dd29b343af8b817002af49
rachidamhouch <rachid.amhouch@gmail.com>
2023-06-06 17:50:00 +0100

    anything

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(str[i]) >= ord('a') and ord(str[i]) <= ord('z'):
+            n = chr(ord(str[i]) - 32)
+        else:
+            n = str[i]
+        print(n, end="")
