815794d3ed3b972a1733d3d323bd2ce57ac6e92e
rachidamhouch <rachid.amhouch@gmail.com>
2023-06-08 20:44:26 +0100

    anything

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import exit, argv
+if __name__ == '__main__':
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if argv[2] not in ops.keys():
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    c = ops[argv[2]](a, b)
+    print("{} {} {} = {}".format(a, argv[2], b, c))
