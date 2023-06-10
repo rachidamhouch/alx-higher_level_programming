e3fdc99724ecd7cefb58d956b12d7d948a320883
rachidamhouch <rachid.amhouch@gmail.com>
2023-06-10 16:43:52 +0100

    anything

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for i in my_string:
+        if ord(i) != ord('c') and ord(i) != ord("C"):
+            new_string += i
+    return new_string
+ 
