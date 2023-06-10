ddbe34e8dc559f26aabb4c7759f2ef96211fc9ad
rachidamhouch <rachid.amhouch@gmail.com>
2023-06-10 16:36:45 +0100

    anything

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,4 +1,4 @@
-def replace_in_list(my_list, idx, element):
+def new_in_list(my_list, idx, element):
     new_list = my_list
     if idx >= 0 and idx < len(my_list):
         new_list[idx] = element
