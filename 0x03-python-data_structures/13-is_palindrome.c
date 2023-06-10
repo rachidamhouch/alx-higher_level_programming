c2132bc943d691091943ab39f60f322f36a53288
rachidamhouch <rachid.amhouch@gmail.com>
2023-06-10 15:08:02 +0100

    anything

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -5,23 +5,20 @@
 int is_palindrome(listint_t **head)
 {
     listint_t   *h = *head, *tmp = NULL;
-    int         i = 0, n = 0;
 
     while (h)
     {
         h->prev = tmp;
         tmp = h;
         h = h->next;
-        i++;
     }
     h = *head;
-    while (n < i / 2)
+    while (h)
     {
         if (h->n != tmp->n)
             return (0);
         h = h->next;
         tmp = tmp->prev;
-        n++;
     }
     return (1);
 }
