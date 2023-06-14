a4bff04a211afffc9f8bba2ddae8a9a5ac51a619
rachidamhouch <rachid.amhouch@gmail.com>
2023-06-14 15:58:45 +0100

    anything

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or not len(a_dictionary):
         return None
     max = list(a_dictionary.keys())[0]
     for i in a_dictionary.keys():
