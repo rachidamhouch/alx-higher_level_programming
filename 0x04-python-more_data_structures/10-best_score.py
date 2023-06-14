3d256294657d57dacf943befc456b7ca089f1ae5
rachidamhouch <rachid.amhouch@gmail.com>
2023-06-14 15:56:38 +0100

    anything

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -7,9 +7,3 @@ def best_score(a_dictionary):
         if a_dictionary[i] > a_dictionary[max]:
             max = i
     return max
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
-
-best_key = best_score(None)
-print("Best score: {}".format(best_key))
