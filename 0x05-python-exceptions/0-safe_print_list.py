8081d3752885ab2787f63e45902ced6f32e55a1f
Rachid amhouch <rachid.amhouch@gmail.com>
2023-06-26 16:57:43 +0100

    anything

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             m += 1
             print(my_list[i], end="")
-    except:
+    except IndexError:
         m -= 1
     print()
     return m
