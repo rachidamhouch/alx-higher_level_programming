c78b456cc4550109713c89244722834d9d060cc6
Rachid amhouch <rachid.amhouch@gmail.com>
2023-06-26 17:15:23 +0100

    anything

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    m = 0
+    try:
+        for i in range(x):
+            m += 1
+            try:
+                print("{:d}".format(my_list[i]), end="")
+            except (ValueError, TypeError):
+                m -= 1
+    except IndexError:
+        m -= 1
+    print()
+    return m
