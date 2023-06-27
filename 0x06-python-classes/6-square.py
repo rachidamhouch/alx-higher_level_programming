958840c920bf8a0e2c67fca308a193ec16fd5e7d
Rachid amhouch <rachid.amhouch@gmail.com>
2023-06-27 22:04:38 +0100

    anything

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,6 +6,10 @@ class Square:
     """Square"""
     def __init__(self, size=0, position=(0, 0)):
         """init"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
         self.__position = position
 
